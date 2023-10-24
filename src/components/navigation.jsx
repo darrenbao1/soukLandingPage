afa32d810b9bf9a638d8775877edf33ff9caff0b
dbao <dbao@seraphlabs.io>
2023-10-24 19:55:07 +0800

    close menu when user clicks any of the links

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-
+import React, { useRef } from "react";
 export const Navigation = (props) => {
+	const navbarRef = useRef(null);
+
+	const handleNavClose = () => {
+		if (navbarRef.current) {
+			navbarRef.current.classList.remove("in"); // Remove the "in" class to close the navbar
+		}
+	};
 	return (
 		<nav id="menu" className="navbar navbar-default navbar-fixed-top">
 			<div className="container">
@@ -16,24 +22,35 @@ export const Navigation = (props) => {
 						<span className="icon-bar"></span>{" "}
 						<span className="icon-bar"></span>{" "}
 					</button>
-					<a className="navbar-brand page-scroll" href="#page-top">
+					<a
+						className="navbar-brand page-scroll"
+						href="#page-top"
+						onClick={() => handleNavClose()}>
 						<img
 							src="img/logo/OldSoukLogo.png"
-							style={{ width: "50%", marginTop: "-15px" }} alt=""></img>
+							style={{ width: "50%", marginTop: "-15px" }}
+							alt=""></img>
 					</a>{" "}
 				</div>
 
 				<div
 					className="collapse navbar-collapse"
-					id="bs-example-navbar-collapse-1">
+					id="bs-example-navbar-collapse-1"
+					ref={navbarRef}>
 					<ul className="nav navbar-nav navbar-right">
 						<li>
-							<a href="#features" className="page-scroll">
+							<a
+								href="#features"
+								className="page-scroll"
+								onClick={() => handleNavClose()}>
 								Features
 							</a>
 						</li>
 						<li>
-							<a href="#about" className="page-scroll">
+							<a
+								href="#about"
+								className="page-scroll"
+								onClick={() => handleNavClose()}>
 								About
 							</a>
 						</li>
@@ -53,12 +70,18 @@ export const Navigation = (props) => {
 							</a>
 						</li> */}
 						<li>
-							<a href="#team" className="page-scroll">
+							<a
+								href="#team"
+								className="page-scroll"
+								onClick={() => handleNavClose()}>
 								Team
 							</a>
 						</li>
 						<li>
-							<a href="#contact" className="page-scroll">
+							<a
+								href="#contact"
+								className="page-scroll"
+								onClick={() => handleNavClose()}>
 								Contact
 							</a>
 						</li>
