5edbb8e1c374a5298f39f324bd7cbeeb3a9a68d6
dbao <dbao@seraphlabs.io>
2023-10-24 17:45:58 +0800

    changing logos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,43 +1,57 @@
 import React from "react";
 
 export const About = (props) => {
-  return (
-    <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>Why Choose Us?</h3>
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="about" className="text-center">
+			<div
+				className="container"
+				style={{ display: "block", alignItems: "center" }}>
+				<div className="col-md-10 col-md-offset-1 section-title">
+					<h2>About</h2>
+				</div>
+				<img
+					src="img/Souk_Architecture.png"
+					className="img-responsive"
+					alt=""
+				/>{" "}
+				{/* 
+					<div className="col-md-6 col-md-20">
+						{" "}
+						<img
+							src="img/Souk_Architecture.png"
+							className="img-responsive"
+							alt=""
+						/>{" "}
+					</div>
+					<div className="col-xs-12 col-md-6">
+						<div className="about-text">
+							<h2>About</h2>
+							<p>{props.data ? props.data.paragraph : "loading..."}</p>
+							<h3>Why Choose Us?</h3>
+							<div className="list-style">
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>
+										{props.data
+											? props.data.Why.map((d, i) => (
+													<li key={`${d}-${i}`}>{d}</li>
+											  ))
+											: "loading"}
+									</ul>
+								</div>
+								<div className="col-lg-6 col-sm-6 col-xs-12">
+									<ul>
+										{props.data
+											? props.data.Why2.map((d, i) => (
+													<li key={`${d}-${i}`}> {d}</li>
+											  ))
+											: "loading"}
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> */}
+			</div>
+		</div>
+	);
 };
--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 
 export const Features = (props) => {
-  return (
-    <div id="features" className="text-center">
-      <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Features</h2>
-        </div>
-        <div className="row">
-          {props.data
-            ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <h3>{d.title}</h3>
-                  <p>{d.text}</p>
-                </div>
-              ))
-            : "Loading..."}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="features" className="text-center">
+			<div className="container">
+				<div className="col-md-10 col-md-offset-1 section-title">
+					<h2>Features</h2>
+				</div>
+				<div className="row">
+					{props.data
+						? props.data.map((d, i) => (
+								<div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+									{" "}
+									{/* check if index 0, will use Cairo_logo.png instead. */}
+									{/* check if index is 3, will use ERC211_logo.png instead */}
+									{i != 0 && i != 3 ? (
+										<i className={d.icon}></i>
+									) : i == 0 ? (
+										<img src="img/logo/Cairo_logo.png" alt="" />
+									) : (
+										<img src="img/logo/ERC2114_logo.png" alt="" />
+									)}
+									<h3>{d.title}</h3>
+									<p>{d.text}</p>
+								</div>
+						  ))
+						: "Loading..."}
+				</div>
+			</div>
+		</div>
+	);
 };
