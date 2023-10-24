d36aa225bb1689323b8ee950b64805baa52a5cbb
dbao <dbao@seraphlabs.io>
2023-10-24 16:26:21 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -13,29 +13,29 @@ import SmoothScroll from "smooth-scroll";
 import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
+	speed: 1000,
+	speedAsDuration: true,
 });
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
+	const [landingPageData, setLandingPageData] = useState({});
+	useEffect(() => {
+		setLandingPageData(JsonData);
+	}, []);
 
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Testimonials data={landingPageData.Testimonials} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
+	return (
+		<div>
+			<Navigation />
+			<Header data={landingPageData.Header} />
+			<Features data={landingPageData.Features} />
+			<About data={landingPageData.About} />
+			{/* <Services data={landingPageData.Services} /> */}
+			{/* <Gallery data={landingPageData.Gallery} /> */}
+			{/* <Testimonials data={landingPageData.Testimonials} /> */}
+			<Team data={landingPageData.Team} />
+			<Contact data={landingPageData.Contact} />
+		</div>
+	);
 };
 
 export default App;
